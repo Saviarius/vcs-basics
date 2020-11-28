059a886adf51be16a9ca66e8105c6d1ca102406a
Saviarius <saviarius@gmail.com>
2020-11-28 17:48:45 +0200

    Initial commit

---
+++ VCSBasics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VCSBasics
+{
+    class Programt
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
